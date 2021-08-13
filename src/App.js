37ea7bb54daafac63676adef1598d7d039152a84
alfu32 <ioan.alferaru@gmail.com>
2021-08-13 07:14:41 +0200

    #1: Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { Layout } from 'components/Layout';
+import { Canvas } from 'components/Canvas';
+import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Layout>
+      <div>Canvas</div>
+      <Canvas />
+    </Layout>
   );
 }
