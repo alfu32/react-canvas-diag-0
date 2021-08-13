4c966a45340924e4007dcb51477eeb6a227720b5
alfu32 <ioan.alferaru@gmail.com>
2021-08-13 08:50:49 +0300

    #1:Layout fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Layout } from 'components/Layout';
-import { Canvas } from 'components/Canvas';
+import { Layout } from './components/Layout';
+import { Canvas } from './components/Canvas';
 import './style.css';
 
 export default function App() {
