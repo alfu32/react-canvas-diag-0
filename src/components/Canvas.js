7ebd91ab2c8529d7818768913a4d3dbcc9774e71
alfu32 <ioan.alferaru@gmail.com>
2021-08-13 18:34:04 +0300

    #1: Canvas drawables

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,11 +3,15 @@ import {useRef,useEffect} from 'react';
 
 export const Canvas = props => {
   const refCanvas = useRef(null);
+  const dwg=props.drawables||[];
   useEffect(()=>{
     const canvas = refCanvas.current;
     const ctx = canvas.getContext('2d');
     console.log({canvas,ctx});
-    ctx.strokeRect(10,10,100,100)
+    ctx.strokeRect(10,10,100,100);
+    dwg.forEach((d)=>{
+      ctx[d.cmd].call(d.args)
+    });
   });
   
   return (
