fdfbb46bd06afc9f2b35a28d50026291a472026e
alfu32 <ioan.alferaru@gmail.com>
2021-08-13 19:37:22 +0300

    #1:Layout status bar and defaults

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
     <Layout>
       <div>Canvas App</div>
       <Canvas />
+      <div>status bar</div>
     </Layout>
   );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,9 +3,9 @@ import React from 'react';
 export const Layout = props => {
   return (
     <div className="app-layout">
-      <div className="app-toolbar">{props.children[0]}</div>
-      <div className="app-content">{props.children[1]}</div>
-      <div className="app-statusbar" />
+      <div className="app-toolbar">{props.children[0]||(<div/>)}</div>
+      <div className="app-content">{props.children[1]||(<div/>)}</div>
+      <div className="app-statusbar" >{props.children[2]||<div/>}</div>
     </div>
   );
 };
