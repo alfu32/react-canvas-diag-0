b83a43a9a3db533a4c86a0a2f0c5d23c4b472186
alfu32 <ioan.alferaru@gmail.com>
2021-08-15 15:30:30 +0300

    #2: Layout Canvas size

--- src/App.js
+++ src/App.js
@@ -29,25 +29,25 @@ export default function App() {
   const [metrics,setMetrics] = useState({count:drawables.length,avg:1});
   let [width, height] = useWindowSize();
   // width-=114;height-=188;
+  const u=5;
+  const w=48;
   const extras = [
-    rectangle({x:0,y:0,width,height})
+    rectangle({x:1,y:1,width:width-3*u-w-4,height:height-4*u-2*w-4})
   ];
   const addDrawable = (ev,synth) => {
     console.log({ev,synth});
     setDrawables([...drawables,point(synth.model)]);
     setMetrics({...metrics,a:(new Date()).getTime(),count:metrics.count + 1})
   };
-  const u=5;
-  const w=48;
   return (
     <>
     <Layout width={width} height={height}
-      margin={5} thickness={48}>
+      margin={u} thickness={w}>
       <Toolbar>Canvas App</Toolbar>
       <ActivityBar></ActivityBar>
       <Canvas
-        width={width-114}
-        height={height-168}
+        width={width-2*u-w-2}
+        height={height-3*u-2*w-2}
         drawables={[...drawables,...extras]}
         onRenderEnd={() => {
           metrics.b = (new Date()).getTime();
