b6b4751558f505f6519edff59a0af865554514d4
alfu32 <ioan.alferaru@gmail.com>
2021-08-15 08:36:36 +0200

    #2: Layout activity bar

---
+++ src/components/ActivityBar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ActivityBar = (props) => {
+  return <>{props.children}</>
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 export const Layout = props => {
+  const toolbar = props.children[0];
+  const content = props.children[1];
+  const status = props.children[2];
+  const activitybar = props.children[3]
   return (
     <div className="app-layout">
-      <div className="app-toolbar">{props.children[0]||(<div/>)}</div>
-      <div className="app-content">{props.children[1]||(<div/>)}</div>
-      <div className="app-statusbar" >{props.children[2]||<div/>}</div>
+      <div className="app-toolbar">{toolbar}</div>
+      <div className="app-activitybar">{activitybar}</div>
+      <div className="app-content">{content}</div>
+      <div className="app-statusbar" >{status}</div>
     </div>
   );
 };
